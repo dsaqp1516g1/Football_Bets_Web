ae98dadcc495b365b4b5c3b0c2411ef367a62157
Marc <marc.bernat@estudiant.upc.edu>
2016-05-02 21:30:20 +0200

    Panel de Usuario (WIP) en tablas

--- Football_API.js
+++ Football_API.js
@@ -27,37 +27,28 @@ function getUsuario() {
     }).done(function(data, status, jqxhr) {
         var html = '';
         html = html.concat('<h3> Información de la cuenta</h3><br>');
-        html = html.concat('<strong> ID: </strong>' + data.id + '<br>');
-        html = html.concat('<strong> LoginID: </strong>' + data.loginid + '<br>');
-        html = html.concat('<strong> Email: </strong>' + data.email + '<br>');
-        html = html.concat('<strong> Balance: </strong>' + data.balance + ' € <br>');
+        html = html.concat('<table class="table table-hover">');
+        html = html.concat('<thead>');
+        html = html.concat('<tbody>');
+        html = html.concat('<tr>');
+        html = html.concat('<th>ID</th>');
+        html = html.concat('<th>LoginID</th>');
+        html = html.concat('<th>Email</th>');
+        html = html.concat('<th>Saldo</th>');
+        html = html.concat('</tr>');
+        html = html.concat('</thead>');
+        html = html.concat('<tbody>');
+        html = html.concat('<tr>');
+        html = html.concat('<td>' + data.id + '</td>');
+        html = html.concat('<td>' + data.loginid + '</td>');
+        html = html.concat('<td>' + data.email + '</td>');
+        html = html.concat('<td>' + data.balance + ' € </td>');
+        html = html.concat('</tr>');
+        html = html.concat('</tbody>');
+        html = html.concat('</table>');
         $("#info_usuario").html(html);
     });
 }
-
-function getUsuario() {
-    var url = API_BASE_URL + '/usuario/' + id;
-    
-    $.ajax({
-        url: url,
-        type: 'GET',
-        headers: {
-            "X-Auth-Token": auth
-        },
-        crossDomain: true,
-        dataType: 'json',
-        
-    }).done(function(data, status, jqxhr) {
-        var html = '';
-        html = html.concat('<h3> Información de la cuenta</h3><br>');
-        html = html.concat('<strong> ID: </strong>' + data.id + '<br>');
-        html = html.concat('<strong> LoginID: </strong>' + data.loginid + '<br>');
-        html = html.concat('<strong> Email: </strong>' + data.email + '<br>');
-        html = html.concat('<strong> Balance: </strong>' + data.balance + ' € <br>');
-        $("#info_usuario").html(html);
-    });
-}
-
 
 function getEquipos() {
     var url = API_BASE_URL + '/equipo/';
@@ -74,17 +65,32 @@ function getEquipos() {
     }).done(function(data, status, jqxhr) {
         
         var html = '';
-        html = html.concat('<img class="icono_para_h3" src="/iconos/football-team-bench.svg"><br>');
+        html = html.concat('<img class="icono_para_h3" align="left" src="/iconos/football-team-bench.svg"><h3>Listado de equipos</h3><br>');
+        html = html.concat('<table class="table table-hover">');
+        html = html.concat('<thead>');
+        html = html.concat('<tbody>');
+        html = html.concat('<tr>');
+        html = html.concat('<th>ID</th>');
+        html = html.concat('<th>Nombre del Equipo</th>');
+        html = html.concat('<th>Nomenclatura</th>');
+        html = html.concat('<th>Valor del Equipo</th>');
+        html = html.concat('</tr>');
+        html = html.concat('</thead>');
+        html = html.concat('<tbody>');
         
         $.each(data.equipos, function(i, data)
         {
-        html = html.concat('<strong> ID: </strong>' + data.id + '<br>');
-        html = html.concat('<strong> LoginID: </strong>' + data.nombre + '<br>');
-        html = html.concat('<strong> Nomenclatura: </strong>' + data.nomenclatura + '<br>');
-        html = html.concat('<strong> Valor: </strong>' + data.valor + ' € <br>');
-        html = html.concat('<br>');
-        
+        html = html.concat('<tr>');
+        html = html.concat('<td>' + data.id + '</td>');
+        html = html.concat('<td>' + data.nombre + '</td>');
+        html = html.concat('<td>' + data.nomenclatura + '</td>');
+        html = html.concat('<td>' + data.valor + ' € </td>');
+        html = html.concat('</tr>');
         })
+        
+        html = html.concat('</tbody>');
+        html = html.concat('</table>');
+        
         $("#info_seleccion").html(html);
     });
 }
